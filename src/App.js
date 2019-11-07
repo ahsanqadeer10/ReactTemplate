945c5ced00b5a11946a037c956f93402b40529b4
ahsanqadeer10 <ahsan.qadeer10@hotmail.com>
2019-11-07 11:24:09 +0500

    React starting template

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import MainContent from './components/MainContent'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <MainContent />
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav>
+            <h1>This is the Navbar!</h1>
+        </nav>
+    )
+}
+
+export default Navbar
